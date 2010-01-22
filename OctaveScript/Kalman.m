edd618c730340e7112798be812367760f1f39c4f
Ben Ballard <bxb107@case.edu>
2010-01-22 14:49:04 -0500

    Got the Kalman filter working.
    Constants need to be worked on. Plus a longer test run should be done to test new features.

--- OctaveScript/Kalman.m
+++ OctaveScript/Kalman.m
@@ -33,8 +33,10 @@ G(1,1)=1;
 G(2,2)=1;
 G(3,3)=1;
 
+P=eye(3).*.001
 
-Vt = zero(3,2);
+
+
 
 
 Diff = 0
@@ -50,13 +52,28 @@ LwP=0
 RwP=0
 
 
+MODELX = 0;
+MODELY =0;
+
+MODELTHETA=0;
+OdomListX = 0
+StateX=0
+StateY=0
+OdomListY=0
 OdomTheta=0
 OdomX=0
 OdomY=0
+YawTheta=0
 OdomListX = 0 
 OdomListY = 0
-
-for x = 2:DataLength 
+ModelX=0;
+ModelY=0;
+modelX=0;
+modelY=0;
+modelTheta=0;
+%DataLength=5
+for x = 2:DataLength
+%for x = 400:500
 	odomX = data{1}(x);
 	odomY = data{2}(x);
 	odomT = data{3}(x);
@@ -86,7 +103,9 @@ for x = 2:DataLength
 	LastTheta =State(3,1);
 	LastX = State(1,1);
 	LastY = State(2,1);
-	
+	%LastY=modelY;
+	%LastX=modelX;
+	%LastTheta=modelTheta;
 %	LastTheta = PredictedState(3,1,x-1);
 %	LastX = PredictedState(1,1,x-1);
 %	LastY = PredictedState(2,1,x-1);	
@@ -94,19 +113,27 @@ for x = 2:DataLength
 	%Do angle prediction
 	dTheta = ((LwP - RwP) * time)/track;
 	modelTheta = dTheta + LastTheta;
+	LastModelTheta = MODELTHETA;
+	MODELTHETA=dTheta+MODELTHETA;
 	if ((RwP-LwP) != 0)
 		
 		ChangeFactor = (track *(LwP + RwP))/(2*(LwP - RwP));
 		modelX = LastX + ChangeFactor*( sin(modelTheta) - sin(LastTheta));
 		modelY = LastY+ ChangeFactor *( cos(modelTheta) - cos(LastTheta));
+		MODELX(x) = MODELX(x-1)+ ChangeFactor*( sin(MODELTHETA) - sin(LastModelTheta));
+		MODELY(x) = MODELY(x-1)+ ChangeFactor*( cos(MODELTHETA) - cos(LastModelTheta));
 		%THIS HEADING MIGHT BE WRONG
-		G(1,3)=ChangeFactor * (-cos(lastTheta)+cos(modelTheta));
-		G(2,3) = ChangeFactor * (-sin(lastTheta)+sin(modelTheta));
+		G(1,3)=ChangeFactor * (-cos(LastTheta)+cos(modelTheta));
+		G(2,3) = ChangeFactor * (-sin(LastTheta)+sin(modelTheta));
 		%Diff = (LastX +RightWS *time*cos(PredictedTheta)) + Diff
 		%R = PredictedX + R;
 	else
 		modelX = LastX + RwP *time*cos(LastTheta);
 		modelY = LastY - RwP*time*sin(LastTheta);
+		MODELX(x) = MODELX(x-1)+ RwP*time*cos(MODELTHETA);
+		MODELY(x) = MODELY(x-1)- RwP*time*sin(MODELTHETA);
+		
+		
 		%THIS HEADING MIGHT BE WRONG
 		G(1,3)= -1*RwP*time*sin(LastTheta);
 		G(2,3) = RwP*time*cos(LastTheta);
@@ -115,67 +142,84 @@ for x = 2:DataLength
 	endif
 	
 	
-	%Note at this point modelTheta may not be witht the appropriate angles
 	
-	model = zeros(3,1)
+	
+	
+	%Note at this point modelTheta may not be witht the appropriate angles
+	%Note this  model solves the Predict the next state
+	model = zeros(3,1);
 	model(1,1) = modelX;
 	model(2,1) = modelY;
 	model(3,1) = modelTheta;
+	state(1,1) = modelX;
+	state(2,1) = modelY;
+	state(3,1) = modelTheta;
 	
 	
-	OdomTheta = Angle(OdomTheta + odomT)
- 	OdomX = OdomX + cos(OdomTheta)*odomX
- 	OdomY = OdomY- sin(OdomTheta)*odomX 
 	
 	
+	%Basic odometry Stuff to comapre the kalman filter against.
+	;OdomTheta = Angle(OdomTheta + odomT);
+ 	OdomX = OdomX+ cos(OdomTheta)*odomX;
+ 	OdomY = OdomY- sin(OdomTheta)*odomX ;
 	OdomListX(x) = OdomX;
 	OdomListY(x) = OdomY;
 	
+	%yaw
+	YawTheta = YawTheta + yawT;
 	
 	
+	%Placed the sensed values into an array
+	%Zsensed(1,1)= odomX+LastX
+	%Zsensed(2,1)= odomY+LastY
+	%Zsensed(3,1)= odomT+LastTheta
+	%Zsensed(4,1)=  yawT+LastTheta
 	
-	PredictedTheta;
-	PredictedX
-	PredictedY
-	PredictedState(1,1,x) = PredictedX;
-	PredictedState(2,1,x) = PredictedY;
-	
-	X = Angle(odomT + X);
-	Z=Z+odomX*cos(X);
-	
-	
-	Zsensed = zeros(4,1)
-	Zsensed(1,1)= odomX;
-	Zsensed(2,1)= odomY;
-	Zsensed(3,1)= odomT;
-	Zsensed(4,1)=  yawT;
-	
-	
-	
-	%plot(x, (request - current) .* ( 1-1./exp(x)).+ current)
-
-	
-	
-	
+	Zsensed(1,1)=OdomX;
+	Zsensed(2,1)= OdomY;
+	Zsensed(3,1)= OdomTheta;
+	Zsensed(4,1)=  YawTheta
 		
-	State(3,1);
-	State(1,1);
-	State(2,1);
+	
+	%This is the Predict the estimated covariance
+	modelSTD=eye(3).*.05;
+	modelSTD(3,3)=.5;
+	P=G*P*G'+modelSTD;
+	Y = Zsensed- H*state;
+	sensorSTD = eye(4).*.005;
+	sensorSTD(3,3)=.3;
+	sensorSTD(4,4) = .04;
+	S = H*P*H'+sensorSTD;
+	K=P*H'*inv(S);
+	state=state+K*Y;
+	P=(eye(3)-K*H)*P
+	
+	%Does this need to be done
+	%state(3,1)=Angle(state(3,1));
 	
 	
 	
 	
-	
-endfor
+	state
+%	PredictedTheta;
+%	PredictedX
+%	PredictedY
+%	PredictedState(1,1,x) = PredictedX;
+%	PredictedState(2,1,x) = PredictedY;
+%	X = Angle(odomT + X);
+%	Z=Z+odomX*cos(X);
 
-BadLeft=0;
-BadRight=0;
-for x = 1:DataLength
-	BadRight = BadRight + abs(RwCD(x));
-	BadLeft = BadLeft + abs(LwCD(x));
+
+
+	StateX(x)= state(1,1);
+	StateY(x)=state(2,1);
+	
+
+	
+	
 endfor
-BadRight;
-BadLeft;
+OdomX
+OdomY
 
 HighLimit = 0.5;
 LowLimit = -0.5;
@@ -184,54 +228,46 @@ LeftLimit = 0
 RightLimit = DataLength
 
 
-figure(1)
-subplot (3, 2, 5)
-plot(LwCD)
-axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
-subplot (3, 2, 6)
-plot(RwCD)
-axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
-subplot (3, 2, 1)
-plot(data{9})
-axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
-subplot (3, 2, 2)
-plot(data{10})
-axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
-subplot (3, 2, 3)
-plot(LwC)
-axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
-subplot (3, 2, 4)
-plot(RwC)
-axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
+%figure(1)
+%subplot (3, 2, 5)
+
+%axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
+%subplot (3, 2, 6)
+
+%axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
+%subplot (3, 2, 1)
+%plot(data{9})
+%axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
+%subplot (3, 2, 2)
+%plot(data{10})
+%axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
+%subplot (3, 2, 3)
+
+%axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
+%subplot (3, 2, 4)
+
+%axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
 
 
 
 
 
-figure(2)
-clf()
+%figure(2)
+%clf()
 %subplot (3, 2, 5)
 %plot(RwCD,'1')
 %axis([0,DataLength,LowLimit,HighLimit]);
 %subplot (3, 2, 6)
 %plot(LwCD,'1')
 %axis([0,DataLength,LowLimit,HighLimit]);
-subplot (1, 2, 2)
-hold on
-plot(RwC,'3')
-plot(RwG,'1')
-plot(RwCD,'4')
-axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
-hold off 
-
-subplot (1, 2, 1)
-hold on
-plot(LwC,'3')
-plot(LwG,'1')
-plot(LwCD,'4')
-hold off
-
-axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
+%subplot (1, 2, 2)
+%hold on
+%axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
+%hold off 
+%subplot (1, 2, 1)
+%hold on
+%hold off
+%axis([LeftLimit,RightLimit,LowLimit,HighLimit]);
 %subplot (3, 2, 3)
 %plot(RwG,'4')
 %axis([0,DataLength,LowLimit,HighLimit]);
@@ -244,7 +280,9 @@ figure(3)
 clf()
 hold on
 %Red
-plot(PredictedState(1,1,:),PredictedState(2,1,:),"*1");
+plot(StateX(:),StateY(:),"*1");
 %Green
 plot(OdomListX(:),OdomListY(:),"*2");
+
+plot(MODELX(:),MODELY(:),"*3");
 hold off
